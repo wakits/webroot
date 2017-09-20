7b7600a90d8a9d822f4562639f9922a6be5ae352
Bruno Lopes <brunolopes@Brunos-MacBook-Pro.local>
2017-09-20 01:00:57 +0100

    added webcli for authetication user

--- webcli/src/app/app.component.spec.ts
+++ webcli/src/app/app.component.spec.ts
@@ -1,27 +1,36 @@
+/* tslint:disable:no-unused-variable */
+
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+      declarations: [ AppComponent ],
+      imports: [ RouterTestingModule ]
+    });
+    TestBed.compileComponents();
+  });
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+
+  it('should render div element with class container', async(() => {
+      let fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      let compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('.container')).toBeDefined();
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+
+  it('should create router-outlet', async(() => {
+      let fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      let compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('router-outlet')).toBeDefined();
   }));
+
 });
